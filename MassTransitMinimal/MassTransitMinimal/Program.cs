89b12ca9094eba3a876561b3459b8e012b2f32ac
Jeff Sternal <jeffsternal@gmail.com>
2019-07-31 07:26:32 -0400

    Initial commit

---
+++ MassTransitMinimal/MassTransitMinimal/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Configuration;
+using System.Threading.Tasks;
+using MassTransit;
+using MassTransit.AzureServiceBusTransport;
+
+namespace MassTransitMinimal
+{
+    public class Program
+    {
+        private static readonly string AzureSbConn = ConfigurationManager.AppSettings["Microsoft.ServiceBus.ConnectionString"];
+
+        public static async Task Main(string[] args)
+        {
+            var bus = Bus.Factory.CreateUsingAzureServiceBus(cfg =>
+            {
+                cfg.Host(AzureSbConn, h => {});
+            });
+
+            bus.Start();
+            bus.Stop();
+
+            Console.WriteLine("Press any key to exit.");
+            await Task.Run(Console.ReadKey);
+        }
+    }
+}
